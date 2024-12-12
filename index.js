726cd2bd8e9bb10bcb800c9dcf3542b3bbccad79
BhushanShinde <bhushannshinde7@gmail.com>
2024-12-12 22:41:23 +0530

    CROS

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-import express from 'express';
-import taskRoutes from './routes/todo.js';
-import connectDB from './config/dbConnection.js';
+import express from "express";
+import taskRoutes from "./routes/todo.js";
+import connectDB from "./config/dbConnection.js";
 
 await connectDB();
 
 const app = express();
 const port = 3000;
 
-app.use(express.json()); 
-app.use('/todo', taskRoutes); 
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the To-Do List API!');
+app.use(express.json());
+app.use("/todo", taskRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the To-Do List API!");
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
